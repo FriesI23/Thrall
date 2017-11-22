37de805d8449d401b6973baf7d8f6ae943c2a1c9
zezhenqin <zezhen.qin@ele.me>
2017-11-22 17:46:15 +0800

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if sys.version_info == (2, 7):
 
 setup(
     name='thrall',
-    version='0.0.2',
+    version='0.0.3',
     description='Maps web-service HTTP Api',
     author='Zoltan Qin',
     author_email='qinzezzhen@outlook.com',
